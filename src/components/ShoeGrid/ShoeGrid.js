ed407de286f4217b09af6c4eb4a76ad4a6a07824
Edmund Chan <edmundcwm@gmail.com>
2022-10-26 17:38:04 +0800

    Style shoe grid

--- src/components/ShoeGrid/ShoeGrid.js
+++ src/components/ShoeGrid/ShoeGrid.js
@@ -8,12 +8,22 @@ const ShoeGrid = () => {
   return (
     <Wrapper>
       {SHOES.map((shoe) => (
-        <ShoeCard key={shoe.slug} {...shoe} />
+        <ShoeWrapper>
+          <ShoeCard key={shoe.slug} {...shoe} />
+        </ShoeWrapper>
       ))}
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  gap: 36px;
+`;
 
+const ShoeWrapper = styled.div`
+  min-width: 275px;
+  flex: 1;
+`;
 export default ShoeGrid;
